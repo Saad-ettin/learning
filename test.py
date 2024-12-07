f49a47062750a399877a9d7d3cb35f2c4210ddef
saadettin <mohammad.saaduddin45@gmail.com>
2024-12-07 10:46:02 +0530

    updated test.py

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-for i in range(10):
-    print(i+1)
+n = int(input("Enter a number : "))
+
+def isPrime (n):
+    if n%2 == 0 :
+        print('Given no. is not prime')
+        return
+    else:
+        for i in range(3,n):
+            if(n%i == 0):
+                print('Given no. is not prime')
+                i = i+1
+                return
+    print('Given number is prime')
+
+isPrime(n)
